a7877722495ddd45ff0efa39b30c2e825c52980f
aylee <miiia@khu.ac.kr>
2022-05-09 02:31:15 +0900

    Fix urls,views,templates and models

--- sort/models.py
+++ sort/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-class list(models.Model):
+class fileSystem(models.Model):
     title = models.CharField(max_length=100)  # 길이 제한이 있는 문자열
-    size = models.TextField()             # 길이 제한이 없는 문자열
+    size = models.IntegerField(default=0)            # 길이 제한이 없는 문자열
     created_at = models.DateTimeField(auto_now_add=True)  # 해당 레코드 생성시 현재 시간 자동
 
     def __str__(self):
--- sort/views.py
+++ sort/views.py
@@ -1,31 +1,20 @@
 from pipes import Template
+from re import L
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
-from .models import list
-
+from .models import fileSystem
+from django.http import HttpResponse
 
 class Home(TemplateView):
-    template_name = 'home.html'
-
-
+    template_name = 'list_sort.html'
+    
 def list_sort(request):
-    '''
-    sort = request.GET.get('sort', '')
-    if sort == 'title':
-        content_list = list.objects.all().order_by('-title', '-created_at')
-    elif sort == 'size':
-        content_list = list.objects.all().order_by('-size', '-created_at')
-    elif sort == 'created_at':
-        content_list = list.objects.all().order_by('-created_at')
-    '''
-    sort = request.Get.get('sort', '')
+    sort = request.GET.get('sort','')
     if sort == 'recent':
-        file_list = list.objects.order_by('created_at', 'created_at')
+        file_list = fileSystem.objects.order_by('created_at', 'created_at')
     elif sort == 'size':
-        file_list = list.objects.order_by('size', 'created_at')
-    elif sort == 'title':
-        file_list = list.objects.order_by('title', 'created_at')
-
-    context = {'file_list': file_list}
-    return render(request, 'sort/list_sort.html', context)
+        file_list = fileSystem.objects.order_by('size', 'created_at')
+    else:
+        file_list = fileSystem.objects.order_by('title', 'created_at')
+    return render(request, 'list_result.html', {'file_list': file_list})
